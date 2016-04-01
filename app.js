de3862b1bedd3902eefd2ff0d00bfe16b6dbdba9
airyland <airyland@qq.com>
2016-04-01 19:23:22 +0800

    update template

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ router.get('/', function *(next) {
 })
 
 router.get('/api/v1/demo.html', function *(next) {
+  if (!this.query.components){
+   this.body = 'No Component Specified~'
+   return
+  }
   this.query.components = this.query.components.split(',').map(function (one){
-    return one.toLowerCase()
+    return {
+      tag: toDash(capitalizeFirstLetter(one)).slice(1),
+      umd: 'vux'+one
+    }
   })
+  this.query.script = this.query.script ? this.query.script.match(/export default \{([\s\S]*?)\}$/)[1]:''
   yield this.render("code", {
     title: "demo",
     data: this.query
@@ -26,8 +34,12 @@ app
   .use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(8082)
+app.listen(7001)
 
 function capitalizeFirstLetter (string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function toDash (str) {
+ return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();})
 }
