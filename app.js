c3a989ba9929547b83719f37b63b6b10b8197e93
airyland <airyland@qq.com>
2016-04-13 13:53:17 +0800

    add filters support

--- app.js
+++ app.js
@@ -11,12 +11,8 @@ router.get('/api/v1/demo.html', function *(next) {
    this.body = 'No Component Specified~'
    return
   }
-  this.query.components = this.query.components.split(',').map(function (one){
-    return {
-      tag: toDash(capitalizeFirstLetter(one)).slice(1),
-      umd: 'vux'+one
-    }
-  })
+  this.query.components = this.query.components.split(',').map(parse)
+  this.query.filters = this.query.filters.split(',').map(parse)
   this.query.script = this.query.script ? this.query.script.match(/export default \{([\s\S]*?)\}$/)[1]:''
   yield this.render("code", {
     title: "demo",
@@ -42,4 +38,11 @@ function capitalizeFirstLetter (string) {
 
 function toDash (str) {
  return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();})
+}
+
+function parse(one){
+  return {
+    tag: toDash(capitalizeFirstLetter(one)).slice(1),
+    umd: 'vux'+one
+  }
 }
